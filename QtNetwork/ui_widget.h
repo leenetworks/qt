e7d6d725cf652af168f5da69cd809e7a2d118fad
lee <leenetworks@163.com>
2012-11-17 14:43:10 +1030

    timer 2D ftp

--- QtNetwork/ui_widget.h
+++ QtNetwork/ui_widget.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'widget.ui'
 **
-** Created: Tue Nov 13 21:33:45 2012
+** Created: Fri Nov 16 00:44:27 2012
 **      by: Qt User Interface Compiler version 4.8.1
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
